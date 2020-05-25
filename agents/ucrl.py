9947602326262da20658e71e88b193b3787def8a
Edouard Leurent <eleurent@gmail.com>
2020-05-25 20:21:12 +0200

    Reward is known by default in UCRL

--- agents/ucrl.py
+++ agents/ucrl.py
@@ -9,8 +9,9 @@ class UCRL(BaseAgent):
     UCRL
     """
     DELTA = 0.1
+    THRESHOLD = 1/DELTA
 
-    def __init__(self, env, horizon, gamma, reward_known=False, **kwargs):
+    def __init__(self, env, horizon, gamma, reward_known=True, **kwargs):
         super().__init__(env, horizon, gamma)
         self.reward_known = reward_known
         self.total_reward = None
@@ -21,7 +22,7 @@ class UCRL(BaseAgent):
     def reset(self):
         super().reset()
         self.total_reward = np.zeros((self.S, self.A))
-        self.reward_ucb = np.zeros((self.S, self.A))
+        self.reward_ucb = np.zeros((self.S, self.A)) if not self.reward_known else self.trueR
         self.q_ucb = np.zeros((self.H, self.S, self.A))
         self.v_ucb = np.zeros((self.H, self.S))
 
@@ -32,38 +33,29 @@ class UCRL(BaseAgent):
         return next_state
 
     def update_reward(self, state, action, reward):
-        self.total_reward[state, action] += reward
+        if not self.reward_known:
+            self.total_reward[state, action] += reward
+            self.reward_ucb[state, action] = kl_upper_bound(self.total_reward[state, action], self.N_sa[state, action],
+                                                            threshold=self.THRESHOLD)
 
     def compute_value_upper_bound(self):
         S, A = self.q_ucb[0, :, :].shape
-        threshold = np.log(1/self.DELTA)
-        if not self.reward_known:
-            for s, a in product(range(S), range(A)):
-                self.reward_ucb[s, a] = kl_upper_bound(self.total_reward[s, a], self.N_sa[s, a], threshold=threshold)
-        else:
-            self.reward_ucb = self.trueR
         for h, s, a in product(range(self.H-1, -1, -1), range(S), range(A)):
             next_v = self.v_ucb[h+1] if h < self.H-1 else np.zeros((S,))
             u_next = self.reward_ucb[s, a] + self.gamma * next_v
             p_plus = max_expectation_under_constraint(u_next, self.P_hat[s, a],
-                                                      threshold / np.maximum(self.N_sa[s, a], 1))
+                                                      self.THRESHOLD / np.maximum(self.N_sa[s, a], 1))
             self.q_ucb[h, s, a] = p_plus @ u_next
             self.v_ucb[h, s] = self.q_ucb[h, s].max()
 
     def run(self, total_samples):
         self.reset()
-        # explore and gather data
         sample_count = 0
         while sample_count < total_samples:
-            # run episode
             state = self.env.reset()
             for hh in range(self.H):
                 sample_count += 1
                 action = self.q_ucb[hh, state, :].argmax()
                 state = self.step(state, action)
-
-            # Compute upper bound
             self.compute_value_upper_bound()
-
-        error = self.estimation_error()
-        return error
+        return self.estimation_error()
