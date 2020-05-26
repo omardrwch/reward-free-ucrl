ecb0911bc57dcacf41db427b15a36bf990cf9960
Omar D <omar.drwch@gmail.com>
2020-05-26 13:58:22 +0200

    update to occupation plot

--- main.py
+++ main.py
@@ -27,6 +27,7 @@ params["clip"] = True
 params["n_runs"]         = 46
 params["n_jobs"]         = 46
 
+
 def estimation_error():
     data = pd.DataFrame(columns=['algorithm', 'samples', 'error', 'error-ucb'])
 
@@ -56,14 +57,51 @@ def estimation_error():
     plot_error(data)
 
 
-def exploration(samples=1000):
-    agent = RandomBaseline(**params)
-    agent.run(samples)
+def show_occupations(samples=1000):
     from matplotlib import pyplot as plt
-    plt.imshow(agent.N_sa.sum(axis=1, keepdims=True).T)
+    from mpl_toolkits.axes_grid1 import ImageGrid
+    import matplotlib.colors as colors
+    del params["clip"]
+    agents = {
+        "RandomBaseline": RandomBaseline(**params),
+        "MB_QVI": MB_QVI(**params),
+        "RF_UCRL with clip": RF_UCRL(**params, clip=True),
+        "RF_UCRL without clip": RF_UCRL(**params, clip=False),
+    }
+
+    plt.figure("occupations")
+    for name, agent in agents.items():
+        agent.run(samples)
+        data = agent.N_sa.sum(axis=1) 
+        plt.semilogy(data, label=name)
+    plt.legend()
     plt.show()
+    
+    # fig = plt.figure(1, (6, 6))
+    # grid = ImageGrid(fig, 111,  # similar to subplot(111)
+    #                  nrows_ncols=(4, 1),
+    #                  direction="row",
+    #                  axes_pad=1,
+    #                  add_all=True,
+    #                  label_mode="1",
+    #                  share_all=True,
+    #                  cbar_location="right",
+    #                  cbar_mode="single",
+    #                  cbar_size="1%",
+    #                  cbar_pad="3%",
+    # )
+    # for ax, (name, agent) in zip(grid, agents.items()):
+    #     agent.run(samples)
+    #     occupations = agent.N_sa.sum(axis=1, keepdims=True).T
+    #     vmax = samples/10
+    #     im = ax.imshow(occupations, origin="lower", interpolation="nearest",
+    #                    norm=colors.SymLogNorm(linthresh=1, linscale=1, vmax=vmax),
+    #                    cmap=plt.cm.coolwarm)
+    #     ax.cax.colorbar(im)
+    #     ax.set_title(name)
+    # plt.show()
 
 
 if __name__=="__main__":
     estimation_error()
-    exploration()
+    show_occupations()
