55fb5d98dcd31f234e2955604059703e1de101d8
Edouard Leurent <eleurent@gmail.com>
2020-05-29 18:14:18 +0200

    Save and load occupancy data

--- state_occupancies.py
+++ state_occupancies.py
@@ -27,19 +27,27 @@ def main():
 
 def show_occupancies(agents: List[Callable], params: dict) -> None:
     print("--- State occupancies ---")
-
-    def occupancies(agent_class: Callable) -> pd.DataFrame:
-        agent = agent_class(**params)
-        agent.run(params["occupancies_samples"])
-        df = pd.DataFrame({"occupancy": agent.N_sa.sum(axis=1),
-                           "state": np.arange(agent.N_sa.shape[0])})
-        df["algorithm"] = agent.name
-        df["samples"] = params["occupancies_samples"]
-        return df
-    output = Parallel(n_jobs=params["n_jobs"], verbose=5)(
-        delayed(occupancies)(agent) for agent, _ in product(agents, range(params["n_runs"])))
-    data = pd.concat(output, ignore_index=True)
+    try:
+        data = pd.read_csv(params["out"] / 'occupancies_data.csv')
+        print("Loaded data from {}.".format(params["out"] / 'occupancies_data.csv'))
+        if data.empty:
+            raise FileNotFoundError
+    except FileNotFoundError:
+        output = Parallel(n_jobs=params["n_jobs"], verbose=5)(
+            delayed(occupancies)(agent, params) for agent, _ in product(agents, range(params["n_runs"])))
+        data = pd.concat(output, ignore_index=True)
+        data.to_csv(params["out"] / 'occupancies_data.csv')
     plot_occupancies(data, params["env"], out_dir=params["out"])
+
+
+def occupancies(agent_class: Callable, params: dict) -> pd.DataFrame:
+    agent = agent_class(**params)
+    agent.run(params["occupancies_samples"])
+    df = pd.DataFrame({"occupancy": agent.N_sa.sum(axis=1),
+                       "state": np.arange(agent.N_sa.shape[0])})
+    df["algorithm"] = agent.name
+    df["samples"] = params["occupancies_samples"]
+    return df
 
 
 if __name__ == "__main__":
