2c015a8df0ce7f55ee311433109dc588dc41f94a
Edouard Leurent <eleurent@gmail.com>
2020-05-31 18:53:23 +0200

    MB-QVI is not allowed more samples than the budget anymore
    Split the budget n into p.SA uniform samples + q random samples

--- agents/mb_qvi.py
+++ agents/mb_qvi.py
@@ -6,6 +6,9 @@ Model-based Q-value iteration (Azar et al., 2012), adapted to the reward free ca
 Sample n transitions from each state-action pair to estimate the model \hat{P},
 then run value iteration with (true_reward, \hat{P}) to estimate the optimal Q-function
 """
+import random
+from itertools import product
+
 import numpy as np
 import pandas as pd
 from agents.base_agent import BaseAgent
@@ -22,11 +25,14 @@ class MB_QVI(BaseAgent):
 
     def run(self, total_samples: int) -> pd.DataFrame:
         self.reset()
-        n_samples_per_pair = int(np.ceil(total_samples/(self.S*self.A)))
-        for ss in range(self.S):
-            for aa in range(self.A):
-                for _ in range(n_samples_per_pair):
-                    # sample transition from (ss, aa)
-                    self.env.reset(ss)   # put env in state ss     <---------  NOT IN GYM, ATTENTION HERE
-                    self.step(ss, aa)
+        # Split budget n into pSA uniform + q random transitions
+        uniform_transitions = list(product(range(self.S), range(self.A), range(total_samples // (self.S*self.A))))
+        supplementary_transitions = list(product(range(self.S), range(self.A), [1]))
+        random.shuffle(supplementary_transitions)
+        supplementary_transitions = supplementary_transitions[:(total_samples % (self.S*self.A))]
+        assert total_samples == len(uniform_transitions) + len(supplementary_transitions)
+        for ss, aa, _ in uniform_transitions + supplementary_transitions:
+            # sample transition from (ss, aa)
+            self.env.reset(ss)   # put env in state ss     <---------  NOT IN GYM, ATTENTION HERE
+            self.step(ss, aa)
         return self.result_dataframe(total_samples)
